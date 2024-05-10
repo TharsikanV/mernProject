8827896ff498016dc36740bf95299d893ff18dd5
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2024-05-10 13:16:06 +0530

    orderDetail implementation

---
+++ .history/frontend1/src/components/order/OrderDetail_20240510123954.js
@@ -0,0 +1,29 @@
+import { Fragment, useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useParams} from 'react-router-dom';
+import {orderDetail as orderDetailAction} from '../../actions/orderActions';
+export default function OrderDetail() {
+const {orderDetail,loading}=useSelector(state=>state.orderState)
+const {
+        shippingInfo={},
+        user={},
+        orderStatus="Processing",
+        orderItems=[],
+        totalPrice=0,
+        paymentInfo={}
+}=orderDetail;
+const isPaid=paymentInfo && paymentInfo.status==="succeeded"?true:false;
+const dispatch=useDispatch();
+const {id}=useParams();
+
+useEffect(()=>{
+    dispatch(orderDetailAction(id))
+},[id])
+
+    return (
+        <Fragment>
+
+        </Fragment>
+        
+    )
+}
